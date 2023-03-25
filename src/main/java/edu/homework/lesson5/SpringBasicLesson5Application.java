9d80e13fadf634453be3f0d5383ddaf0b232693b
iuad0qak <ios.developers@aval.ua>
2023-03-25 18:35:15 +0200

    Rename package lesson4 to lesson5/

--- src/main/java/edu/homework/lesson5/SpringBasicLesson5Application.java
+++ src/main/java/edu/homework/lesson5/SpringBasicLesson5Application.java
@@ -1,4 +1,4 @@
-package edu.homework.lesson4;
+package edu.homework.lesson5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
